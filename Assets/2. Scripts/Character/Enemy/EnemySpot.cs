cd0f14abca58a1c827a8e89b32c524ae59180ac3
woomay0608 <woohuck0608@naver.com>
2025-03-12 20:44:09 +0900

    [Feat] 특정 영역 나갈 시 몬스터 삭제

--- Assets/2. Scripts/Character/Enemy/EnemySpot.cs
+++ Assets/2. Scripts/Character/Enemy/EnemySpot.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpot : MonoBehaviour
 {
@@ -11,11 +12,11 @@ public class EnemySpot : MonoBehaviour
     void Start()
     {
         Surface = GetComponent<NavMeshSurface>();
-        
 
-        for (int i = 0; i < transform.parent.childCount-1; i++) 
+
+        for (int i = 0; i < transform.parent.childCount - 1; i++)
         {
-            EnemyObjects.Add(transform.parent.transform.GetChild(i+1).GetComponent<EnemyObject>());
+            EnemyObjects.Add(transform.parent.transform.GetChild(i + 1).GetComponent<EnemyObject>());
         }
 
     }
@@ -31,14 +32,26 @@ public class EnemySpot : MonoBehaviour
             {
                 EnemyObjects[i].transform.localScale = new Vector3(1f / transform.parent.transform.localScale.x,
                     1f / transform.parent.localScale.y, 1f / transform.parent.transform.localScale.z);
-                EnemyObjects[i].transform.position = new Vector3(Random.Range(transform.parent.position.x - transform.parent.localScale.x/2,
-                    transform.parent.position.x + transform.parent.localScale.x /2), transform.position.y, Random.Range(transform.parent.position.z - transform.parent.localScale.z / 2,
+                EnemyObjects[i].transform.position = new Vector3(Random.Range(transform.parent.position.x - transform.parent.localScale.x / 2,
+                    transform.parent.position.x + transform.parent.localScale.x / 2), transform.position.y, Random.Range(transform.parent.position.z - transform.parent.localScale.z / 2,
                     transform.parent.position.z + transform.parent.localScale.z / 2));
                 EnemyObjects[i].gameObject.SetActive(true);
             }
         }
     }
-   
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            for (int i = 0; i < transform.parent.childCount - 1; i++)
+            {
+                EnemyObjects[i].gameObject.SetActive(false);
+            }
+            Surface.navMeshData = null;
+        }
 
 
+
+    }
 }
