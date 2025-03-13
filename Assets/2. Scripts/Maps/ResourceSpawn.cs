8d89beb51c6e1cbec9c8865829eebc27456fa751
woomay0608 <woohuck0608@naver.com>
2025-03-13 14:54:55 +0900

    [chore] 어디선가 날라옴

---
+++ Assets/2. Scripts/Maps/ResourceSpawn.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceSpawn : MonoBehaviour
+{
+    public List<StoneResource> StoneSpawnPosition;
+    public List<WoodResource> WoodSpawnPosition;
+
+    [Range(0f, 1f)] public float SpawnPercentage;
+
+    int HowManyStoneSpawn;
+    int HowManyWoodSpawn;
+
+    private void Start()
+    {
+        Invoke("LateStart", 0.1f);
+    }
+
+    void LateStart()
+    {
+        ChangeSpawnPercentage();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            SpawnResource();
+        }
+    }
+
+    public void ChangeSpawnPercentage()
+    {
+        HowManyStoneSpawn = Mathf.RoundToInt(SpawnPercentage * StoneSpawnPosition.Count);
+        HowManyWoodSpawn = Mathf.RoundToInt(SpawnPercentage * WoodSpawnPosition.Count);
+    }
+
+    public void SpawnResource()
+    {
+        BreakAllResource();
+
+        int[] StoneSpawnNum = new int[StoneSpawnPosition.Count];
+        int[] WoodSpawnNum = new int[WoodSpawnPosition.Count];
+
+        for (int i = 0; i < StoneSpawnPosition.Count; i++)
+        {
+            StoneSpawnNum[i] = i;
+        }
+        for (int i = 0;i < WoodSpawnPosition.Count; i++)
+        {
+            WoodSpawnNum[i] = i;
+        }
+
+        for (int i = 0; i < HowManyStoneSpawn; i++)
+        {
+            int random = Random.Range(i, StoneSpawnPosition.Count);
+
+            StoneSpawnPosition[random].SpawnResource();
+
+            StoneSpawnNum[random] = StoneSpawnNum[i];
+        }
+        for (int i = 0; i < HowManyWoodSpawn; i++)
+        {
+            int random = Random.Range(i, WoodSpawnPosition.Count);
+
+            WoodSpawnPosition[random].SpawnResource();
+
+            WoodSpawnNum[random] = WoodSpawnNum[i];
+        }
+    }
+
+    void BreakAllResource()
+    {
+        foreach (StoneResource stone in StoneSpawnPosition)
+        {
+            stone.DeleteResource();
+        }
+        foreach (WoodResource wood in WoodSpawnPosition)
+        {
+            wood.DeleteResource();
+        }
+    }
+}
