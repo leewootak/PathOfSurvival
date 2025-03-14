dc796d2c96f8690bf64578cc69bbd89fba69d996
leewootak <leewt32414@gmail.com>
2025-03-14 21:38:04 +0900

    [Fix] GameManager와 UIManager 및 PlayerController 연결

--- Assets/2. Scripts/Manager/GameManager.cs
+++ Assets/2. Scripts/Manager/GameManager.cs
@@ -7,15 +7,15 @@ public class GameManager : MonoBehaviour
     static GameManager gameManager;
 
     public static GameManager Instance { get { return gameManager; } }
-    //public PlayerController player { get; private set; }
+    public PlayerController player { get; private set; }
 
-    //UIManager uiManager;
+    UIManager uiManager;
 
     private void Awake()
     {
         gameManager = this; // 싱글톤 인스턴스로 현재 객체 할당
         //player = FindObjectOfType<PlayerController>();
-        //uiManager = FindObjectOfType<UIManager>();
+        uiManager = FindObjectOfType<UIManager>();
     }
 
     private void Start()
