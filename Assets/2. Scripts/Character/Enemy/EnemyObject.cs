2fe04d887adfbc0a96b395d3166ee19ef8ccb73e
woomay0608 <woohuck0608@naver.com>
2025-03-17 12:02:55 +0900

    [feat] ItemDrop 추가

--- Assets/2. Scripts/Character/Enemy/EnemyObject.cs
+++ Assets/2. Scripts/Character/Enemy/EnemyObject.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class EnemyObject : MonoBehaviour
 {
     [SerializeField] private EnemyInfo EnemyInfo;
-
+    [SerializeField] private List<ItemData> DropItem;
     private Animator animator;
     private float health;
 
     public void Set()
     {
         health = EnemyInfo.Health;
+        DropItemSet(DropItem);
     }
     private void Start()
     {
@@ -38,9 +39,45 @@ public class EnemyObject : MonoBehaviour
     private IEnumerator Die()
     {
         animator.SetTrigger("DIE");
+        Drop();
         yield return new WaitForSeconds(2f);
         gameObject.SetActive(false);
         EnemyPool.Instance.ReQueue(gameObject);
+    }
+
+    private void DropItemSet(List<ItemData> items)
+    {
+        int Persentage  = Random.Range(0, 100);
+
+        if(Persentage <5)
+        {
+            items.Add(ItemManager.Instance.list[0]);
+        }
+        if(Persentage < 40)
+        {
+            items.Add(ItemManager.Instance.list[1]);
+        }
+        if (Persentage < 60)
+        {
+            items.Add(ItemManager.Instance.list[2]);
+        }
+        if (Persentage % 2 == 1)
+        {
+            items.Add(ItemManager.Instance.list[3]);
+        }
+
+    }
+
+    private void Drop()
+    {
+        foreach(ItemData item in DropItem)
+        {
+           GameObject DropItem = Instantiate(item.dropPrefab, transform);
+            Vector3 RandomDropPosition = new Vector3(transform.position.x+ Random.Range(-1f,1f), 
+                transform.position.y, transform.position.z + Random.Range(-1f,1f));
+            DropItem.transform.position = RandomDropPosition;
+            DropItem.transform.rotation = Quaternion.identity;
+        }
     }
     
 }
