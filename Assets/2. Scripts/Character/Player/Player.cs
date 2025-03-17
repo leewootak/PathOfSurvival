86df349a3695684f286bf77eb713b6375db8c68c
tmdwp <tmdwp1010@gmail.com>
2025-03-17 16:44:38 +0900

    [Test] 플레이어 인벤토리 연결

--- Assets/2. Scripts/Character/Player/Player.cs
+++ Assets/2. Scripts/Character/Player/Player.cs
@@ -6,11 +6,12 @@ public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
-
+    //public Equipment equip;
     private void Awake()
     {
         CharacterManager.Instance.Player = this; //CharacterManager에 있는 Player 에 Player 를 넣는다
         controller = GetComponent<PlayerController>();
         condition = GetComponent<PlayerCondition>();
+    //    equip = GetComponent<Equipment>();
     }
 }
