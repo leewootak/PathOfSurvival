346d449908f3882480bc4704b52e516a9e3fb1af
leewootak <leewt32414@gmail.com>
2025-03-18 23:39:24 +0900

    [Chore] 불필요한 폴더 삭제

--- Assets/2. Scripts/UI/CraftTable.cs
+++ Assets/2. Scripts/UI/CraftTable.cs
@@ -15,7 +15,7 @@ public class CraftTable : MonoBehaviour
     // 배치 모드 활성 여부
     private bool IsSelect = false;
     // 배치 가능 상태 여부
-    public bool CanPlace;
+    public bool CanPlace = false;
 
     [SerializeField] private Build_Prefabs build_Prefabs;
 
@@ -26,6 +26,7 @@ public class CraftTable : MonoBehaviour
         cam = FindAnyObjectByType<Camera>();
         player = FindAnyObjectByType<Player>();
         layerMask = LayerMask.GetMask("Buildable");
+
     }
 
     private void Start()
@@ -35,6 +36,7 @@ public class CraftTable : MonoBehaviour
 
     private void Update()
     {
+        Place();
     }
 
     private void FixedUpdate()
@@ -53,9 +55,8 @@ public class CraftTable : MonoBehaviour
             // 아이템 프리팹 생성
             Instantiate(prefabs, prefabs.transform.position, Quaternion.identity);
         }
-
+        
         IsSelect = true;
-        CanPlace = true;
         build_Prefabs.IsPlaced = false;
 
         Debug.Log("부품 선택");
@@ -70,10 +71,9 @@ public class CraftTable : MonoBehaviour
             // 10 이내의 그라운드를 감지하고, 플레이어와의 거리가 10 미만일 때
             if (Physics.Raycast(ray, out hit, 15f, layerMask) && Vector3.Distance(player.transform.position, hit.point) < 13f)
             {
-                CanPlace = true;
-                
                 if (hit.collider != null)
                 {
+                    CanPlace = true;
 
                     // 배치 가능 상태 색상으로 변경
                     prefabs.transform.GetChild(0).GetComponent<Build_Prefabs>().ColorChange(2);
@@ -107,15 +107,15 @@ public class CraftTable : MonoBehaviour
     // 아이템 배치 및 회전 (클릭 계속하면 디버그 로그 계속 찍히는 현상 수정 필요)
     private void Place()
     {
-        if (CanPlace = Input.GetMouseButtonDown(0))
+        if (CanPlace && Input.GetMouseButtonDown(0))
         {
             IsSelect = false;
             prefabs.transform.GetChild(0).GetComponent<Rigidbody>().isKinematic = true;
 
             // 배치 완료 상태 색상으로 변경
             prefabs.transform.GetChild(0).GetComponent<Build_Prefabs>().ColorChange(0);
-            prefabs.layer = 10;
-            prefabs.transform.GetChild(0).gameObject.layer = 10;
+            prefabs.layer = 7;
+            prefabs.transform.GetChild(0).gameObject.layer = 7;
 
             build_Prefabs.IsPlaced = true;
             Debug.Log("배치");
