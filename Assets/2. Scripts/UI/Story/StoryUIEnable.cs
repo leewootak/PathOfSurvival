add5f941cad50aa39d767f3adf8ee802811daa14
Yun-Jiyeol <yir0612@gmail.com>
2025-03-18 12:34:43 +0900

    [Feat] 스토리UI 높이 조정, 스토리 추가

--- Assets/2. Scripts/UI/Story/StoryUIEnable.cs
+++ Assets/2. Scripts/UI/Story/StoryUIEnable.cs
@@ -6,6 +6,7 @@ public class StoryUIEnable : MonoBehaviour
 {
     public GameObject StoryOneLine;
     public Transform SpawnPosition;
+    public RectTransform SpawnStoryHeight;
 
     private void OnEnable()
     {
@@ -13,6 +14,8 @@ public class StoryUIEnable : MonoBehaviour
         {
             if (it.isPlayerHave && !it.isShow)
             {
+                SpawnStoryHeight.sizeDelta = new Vector2(SpawnStoryHeight.sizeDelta.x, SpawnStoryHeight.sizeDelta.y + 140);
+
                 GameObject go = Instantiate(StoryOneLine);
                 go.transform.parent = SpawnPosition;
                 go.GetComponent<StoryOneLine>().postIt = it;
