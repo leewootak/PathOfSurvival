ed224621c41b07a66571d4085458e056d957c2b4
woomay0608 <woohuck0608@naver.com>
2025-03-18 17:04:59 +0900

    [chore] 안 쓰는 캔버스 삭제

--- Assets/2. Scripts/UI/CraftTable.cs
+++ Assets/2. Scripts/UI/CraftTable.cs
@@ -24,7 +24,6 @@ public class CraftTable : MonoBehaviour
     {
         UI = transform.parent.gameObject;
         cam = FindAnyObjectByType<Camera>();
-
         Player = FindAnyObjectByType<Player>();
 
 
