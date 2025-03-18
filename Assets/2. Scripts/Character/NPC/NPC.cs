c044e05ec20f004702a3ef7aee3271d680597f7b
woomay0608 <woohuck0608@naver.com>
2025-03-18 15:03:27 +0900

    [chore] 아이템 수정

--- Assets/2. Scripts/Character/NPC/NPC.cs
+++ Assets/2. Scripts/Character/NPC/NPC.cs
@@ -5,34 +5,15 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    private GameObject[] interactObject;
+    public GameObject Clear;
 
-    private void Start()
+    private void OnCollisionEnter(Collision collision)
     {
-        interactObject = GetComponentsInChildren<GameObject>();
-    }
-
-
-    public void TalkToMeTextDown()
-    {
-        interactObject[1].gameObject.SetActive(false);
-    }
-    public  void DescriptionTextDown()
-    {
-        interactObject[0].gameObject.SetActive(false);
-    }
-    public void DescriptionTextOn()
-    {
-        interactObject[0].gameObject.SetActive(true);
-    }
-    public void FriendsAvataDown()
-    {
-        interactObject[2].gameObject.SetActive(false);
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Clear.SetActive(true);
+        }
     }
 
-    public Transform CameraMoving()
-    {
-        return interactObject[3].transform;
-    }
 
 }
