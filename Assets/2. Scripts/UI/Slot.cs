4aa15732ed167c60b717157dda29c80e58dd6971
Yun-Jiyeol <yir0612@gmail.com>
2025-03-19 09:45:00 +0900

    [Feat] 식수, 벽, 토치 제작 가능

--- Assets/2. Scripts/UI/Slot.cs
+++ Assets/2. Scripts/UI/Slot.cs
@@ -14,29 +14,4 @@ public class Slot : MonoBehaviour
         Itemicon.sprite = image;
         NumObItems.text = quantity.ToString();
     }
-
-    public void InventorySlot()
-    {
-        Button button = gameObject.AddComponent<Button>();
-        button.onClick.AddListener(Inventorybutton);
-    }
-
-    void Inventorybutton()
-    {
-        //아이템 데이터 타입에 따른 효과
-        //consumable면 그에 맞는 스탯 변경
-        //장착,설치 가능시 장착하고 있는 물건 제거 및 장착
-        //재료면 return
-    }
-
-    public void EquipSlot()
-    {
-        Button button = gameObject.AddComponent<Button>();
-        button.onClick.AddListener(Equipbutton);
-    }
-
-    void Equipbutton()
-    {
-        //장착하고 있는 물건을 제거
-    }
 }
